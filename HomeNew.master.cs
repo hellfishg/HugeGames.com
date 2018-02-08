1dd902c13cc8700d65a6f3d2b216eaad20fc642b
Diego Silvera de Souza <ctk.591@gmail.com>
2018-02-08 01:17:31 -0300

    se agrega validacion de usuario logeado para mostrar boton de perfil o de login

--- HomeNew.master.cs
+++ HomeNew.master.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class HomeNew : System.Web.UI.MasterPage
 {
+    public clsUsuario Usr = new clsUsuario();
     protected void Page_Load(object sender, EventArgs e)
     {
+        var session = Session["sUser"];
 
+        if (session != null)
+        {
+            Usr = (clsUsuario)session;
+        }
     }
 }
